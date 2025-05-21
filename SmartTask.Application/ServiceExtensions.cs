a053349a8d5c4ab746011df38e8dd0529de07ee9
Durosinmi Abdul-Sobuur <durosinmiabdulsobuur@gmail.com>
2025-05-21 16:09:27 +0100

    Added Project Files

---
+++ SmartTask.Application/ServiceExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartTask.Application
+{
+    public class ServiceExtensions
+    {
+    }
+}

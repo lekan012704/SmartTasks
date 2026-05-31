924371d837a6016e3b773985dd4a483affc5722c
Lekan Abidemi <lekan012704@gmail.com>
2026-05-31 12:30:16 +0100

    uodate changes

--- SmartTask.Api/Program.cs
+++ SmartTask.Api/Program.cs
@@ -39,7 +39,7 @@ namespace SmartTask.Api
                 options.AddDefaultPolicy(policy =>
                 {
                     policy.WithOrigins(
-                            builder.Configuration["AllowedOrigins"] ?? "https://smart-seller-frontend.vercel.app/"
+                            builder.Configuration["AllowedOrigins"] ?? "http://localhost:3000"
                           )
                           .AllowAnyHeader()
                           .AllowAnyMethod()
